c11b1ccaa92c1f8011269a1f7ea425976b511208
dishowrov <dinislamshowrov2001@gmail.com>
2024-05-31 02:56:25 +0600

    designed 3 pages

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,7 +2,7 @@
 // Home page's hero slider section
 $('.home-hero-slider').slick({
   arrows: true,
-  dots: true,
+  dots: false,
   initialSlide: 0,
   infinite: true,
   slidesToShow: 1,
@@ -17,9 +17,9 @@ $('.home-hero-slider').slick({
   pauseOnFocus: false,
   pauseOnHover: false,
   prevArrow:
-    "<button type='button' class='slick-prev'> agor </button>",
+    "<button type='button' class='slick-arrow prev-arrow'> <i class='fa-solid fa-angle-left'></i> </button>",
   nextArrow:
-    "<button type='button' class='slick-next'> bador </button>",
+    "<button type='button' class='slick-arrow next-arrow'> <i class='fa-solid fa-angle-right'></i> </button>",
   responsive: [
     {
       breakpoint: 850,
@@ -60,13 +60,43 @@ $(".home-hero-slider").on(
   }
 );
 
+
+// Home page's Reviews slider
+$('.slider-for').slick({
+  slidesToShow: 1,
+  arrows: false,
+  asNavFor: '.slider-nav',
+  infinite: true,
+  fade: true
+});
+
+$('.slider-nav').slick({
+  slidesToShow: 2,
+  asNavFor: '.slider-for',
+  focusOnSelect: true,
+  autoplay: false,
+  infinite: true,
+  arrows: true,
+  prevArrow:
+    "<button type='button' class='slick-arrow prev-arrow'> <i class='fa-solid fa-angle-left'></i> </button>",
+  nextArrow:
+    "<button type='button' class='slick-arrow next-arrow'> <i class='fa-solid fa-angle-right'></i> </button>",
+});
+
 // Home page's New Arrival section's functionality
 $('.new-arrivals-slider').slick({
-  dots: false,
+  dots: true,
+  arrows: true,
   slidesToShow: 4,
   slidesToScroll: 1,
+  infinite: true,
   autoplay: true,
-  autoplaySpeed: 4500
+  autoplaySpeed: 4500,
+  cssEase: "cubic-bezier(0.7, 0, 0.3, 1)",
+  prevArrow:
+    "<button type='button' class='slick-arrow prev-arrow'> <i class='fa-solid fa-angle-left'></i> </button>",
+  nextArrow:
+    "<button type='button' class='slick-arrow next-arrow'> <i class='fa-solid fa-angle-right'></i> </button>",
 });
 
 
